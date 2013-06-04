e1923550e406e950b2a9d686a81a86d07f476e5b
webdorado <webdorado@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-04 14:30:26 +0000

    bug fixed
    git-svn-id: https://plugins.svn.wordpress.org/catalog/tags/1.4.9@722587 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ elements/ratingstars.php
@@ -0,0 +1,57 @@
+<?php /** * @package Spider Catalog * @author Web-Dorado * @copyright (C) 2012 Web-Dorado. All rights reserved. * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html **/defined('_JEXEC') or die('Restricted access');
+
+class JElementRatingstars extends JElement
+
+{
+
+function fetchElement($name, $value, &$node, $control_name)
+
+{
+
+        ob_start();
+		
+        $img=JUri::root()."components/com_spidershop/images/";
+
+            ?>
+
+   
+<table cellpadding="0" cellspacing="10" border="0" ><tr>
+<td>
+<img src="<?php echo $img;?>star1.png" alt="star1" /><br />
+<input name="<?php echo $control_name."[".$name."]";?>" type="radio" class="inputbox" id="<?php echo  $control_name.$name ?>" value="1" size="10" <?php if($value==1) echo 'checked="checked"'; ?> />
+</td><td>
+<img src="<?php echo $img;?>star2.png" alt="star2" /><br />
+<input name="<?php echo $control_name."[".$name."]";?>" type="radio" class="inputbox" id="<?php echo  $control_name.$name ?>" value="2" size="10" <?php if($value==2) echo 'checked="checked"'; ?> />
+</td><td>
+<img src="<?php echo $img;?>star3.png" alt="star3" /><br />
+<input name="<?php echo $control_name."[".$name."]";?>" type="radio" class="inputbox" id="<?php echo  $control_name.$name ?>" value="3" size="10" <?php if($value==3) echo 'checked="checked"'; ?> />
+</td><td>
+<img src="<?php echo $img;?>star4.png" alt="star4" /><br />
+<input name="<?php echo $control_name."[".$name."]";?>" type="radio" class="inputbox" id="<?php echo  $control_name.$name ?>" value="4" size="10" <?php if($value==4) echo 'checked="checked"'; ?> />
+</td><td>
+<img src="<?php echo $img;?>star5.png" alt="star5" /><br />
+<input name="<?php echo $control_name."[".$name."]";?>" type="radio" class="inputbox" id="<?php echo  $control_name.$name ?>" value="5" size="10" <?php if($value==5) echo 'checked="checked"'; ?> />
+</td></tr>
+</table>
+
+        <?php
+
+
+
+        $content=ob_get_contents();
+
+
+
+        ob_end_clean();
+
+        return $content;
+
+
+
+    }
+
+	}
+
+	
+
+	?>
