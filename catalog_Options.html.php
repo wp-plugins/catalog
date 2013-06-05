99f1eb503d9e8c6517100a841e929a604eed28ee
webdorado <webdorado@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-05 10:58:29 +0000

    bug fixed
    git-svn-id: https://plugins.svn.wordpress.org/catalog/tags/1.5.0@723027 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ catalog_Options.html.php
@@ -0,0 +1,125 @@
+	<?php	
+	
+	if(!current_user_can('manage_options')) {
+	die('Access Denied');
+}	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+function html_showGlobal( $param_values,  $op_type){
+
+
+
+
+?>
+<div class="updated" style="font-size: 14px; color:red !important"><p><strong>Global Options is disabled in free version. If you need this functionality, you need to buy the commercial version.</strong></p></div>
+<table width="80%">
+  <tbody>
+                <tr>   
+<td width="100%" style="font-size:14px; font-weight:bold"><a href="http://web-dorado.com/spider-catalog-wordpress-guide-step-4/spider-catalog-wordpress-guide-step-4-1.html" target="_blank" style="color:blue; text-decoration:none;">User Manual</a><br />
+This section allows you to configure the global options. <a href="http://web-dorado.com/spider-catalog-wordpress-guide-step-4/spider-catalog-wordpress-guide-step-4-1.html" target="_blank" style="color:blue; text-decoration:none;">More...</a><br />
+If you want to customize to the global options of your website,than you need to buy a license</td>   
+<td colspan="7" align="right" style="font-size:16px;">
+  <a href="http://web-dorado.com/files/fromSpiderCatalog.php" target="_blank" style="color:red; text-decoration:none;">
+<img src="<?php echo plugins_url("images/header.png",__FILE__) ?>" border="0" alt="http://web-dorado.com/files/fromSpiderCatalog.php" width="215"><br>
+Get the full version&nbsp;&nbsp;&nbsp;&nbsp;
+</a>
+  </td>
+        </tr>
+
+
+  </tbody></table>
+<img src="<?php echo plugins_url("images/WPCatalog-global.png",__FILE__) ?>" >
+<?php
+      }
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+function	  html_showStyles( $param_values,$op_type){
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+?>
+<div class="updated" style="font-size: 14px; color:red !important"><p><strong>Styles and Colors is disabled in free version. If you need this functionality, you need to buy the commercial version.</strong></p></div>
+<table width="60%">
+  <tbody>
+  <tr>      
+  <td colspan="7" align="right" style="font-size:16px;">
+  <a href="http://web-dorado.com/files/fromSpiderCatalog.php" target="_blank" style="color:red; text-decoration:none;">
+<img src="<?php echo plugins_url("images/header.png",__FILE__) ?>" border="0" alt="http://web-dorado.com/files/fromSpiderCatalog.php" width="215"><br>
+Get the full version&nbsp;&nbsp;&nbsp;&nbsp;
+</a>
+  </td></tr>
+  
+  </tbody></table>
+
+<br />
+<img src="<?php echo plugins_url("images/WPCatalog.png",__FILE__) ?>" >
+
+<?php
+      }
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
